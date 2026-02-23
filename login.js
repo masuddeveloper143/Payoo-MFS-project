d0082ab321f38d91bad31b4b4a79accc5d26f9ff
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-24 01:20:10 +0800

    first commit

---
+++ login.js
@@ -0,0 +1,40 @@
+// alert("allow the permition");
+// console.log('masud rana');
+
+document.getElementById('btn-login')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        // console.log('hello masud');
+        let inputNumber = document.getElementById('input-number').value;
+        let inputDigit = document.getElementById('input-digit').value;
+        // console.log('Account Number: ', inputNumber);
+        // console.log('OTP: ', inputDigit);
+
+        if (inputNumber.length === 11) {
+            // console.log('number succesfull');
+            if (inputDigit === 4) {
+                console.log('corrct your pin');
+            }
+
+            else {
+                console.log('invalid your pin');
+            }
+
+        }
+        else {
+            console.log('invalid your number');
+        }
+
+        // else {
+        //     console.log('incorrect number', inputNumber);
+        // };
+
+        // if (inputDigit.length === 4) {
+        //     console.log('OTP succesfull');
+        // }
+
+
+        // else {
+        //     console.log('incorrect OTP');
+        // };
+    })
