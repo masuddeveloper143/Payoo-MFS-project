437870d7a00774e063157e7cc2b821a1e98ef70b
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-26 01:27:43 +0800

    Update project

---
+++ recep.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-add-money')
+.addEventListener('click', function(event){
+    // console.log('button cliked');
+    event.preventDefault();
+
+    const amount = document.getElementById()
+})
