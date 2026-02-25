43cdaf758a27f356a53a933e37040bf81dbc8ac9
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-25 15:20:31 +0800

    Update project

--- add_money.js
+++ add_money.js
@@ -4,6 +4,23 @@ document.getElementById('btn-add-money')
         const amount = document.getElementById('amount').value;
         // console.log(typeof amount);
         const convertedAmount = parseFloat(amount);
-        document.getElementById('pin').value;
-        
+        const pin = document.getElementById('pin').value;
+        const convertedPin = parseFloat(pin);
+        const minBalance = document.getElementById('min_balence')
+            .innerText;
+        // console.log(typeof minBalance);
+        const convertedBalance = parseFloat(minBalance);
+        // console.log(typeof convertedBalance);
+
+        if (convertedPin === 1234) {
+            // console.log('Add money succsesfule')
+            const sum = convertedBalance + convertedAmount;
+            minBalance = document.getElementById('min_balence').innerText = sum;
+            
+        }
+        else {
+            console.log('incrroct your Pin');
+
+        }
+
     })
