e7f01cf809b29e32c93e37c06d54ef41bf8c68d5
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-27 02:14:32 +0800

    Update project

--- cashout/cashout.js
+++ cashout/cashout.js
@@ -3,29 +3,29 @@ document.getElementById('cashout_btn')
         even.preventDefault();
 
         const accountNumber = document.getElementById('acount_number').value;
-        const pin = document.getElementById('pin').value;
-        const amount = document.getElementById('amount').value;
+        const pin = document.getElementById('cashout_pin').value;
+        const amount = document.getElementById('cashout_pin').value;
         const minBalance = document.getElementById('min_balence').innerText;
 
         //    string convert to Number 
         const convertedToNumber = parseFloat(accountNumber);
         const convertedToPin = parseFloat(pin);
         const convertedToAmount = parseFloat(amount);
-        const convertedToMinBalance = parseFloat(minBalance)
+        const convertedToMinBalance = parseFloat(minBalance);
 
         if (convertedToPin === 1234) {
+
+            if (convertedToAmount > convertedToMinBalance) {
+                alert("Insufficient Balance");
+                return;
+            }
+
             const sum = convertedToMinBalance - convertedToAmount;
             document.getElementById('min_balence').innerText = sum;
-            // if (convertedToNumber === 11) {
-            //     alert('successfull')
-            // }
 
-            // else {
-            //     alert('incorrct your number');
-            // }
-        }
+            alert("Cash Out Successful");
 
-        else {
-            alert('incorrct your pin');
+        } else {
+            alert('Incorrect Pin');
         }
     });
