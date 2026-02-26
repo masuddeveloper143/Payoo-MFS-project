91bade19de618ab80c2c11e7d02b3c0ea733a522
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-27 01:45:45 +0800

    Update project

--- cashout/cashout.js
+++ cashout/cashout.js
@@ -13,8 +13,19 @@ document.getElementById('cashout_btn')
         const convertedToAmount = parseFloat(amount);
         const convertedToMinBalance = parseFloat(minBalance)
 
-        if (convertedToPin === 6644) {
-            const sum = convertedToAmount - convertedToMinBalance;
+        if (convertedToPin === 1234) {
+            const sum = convertedToMinBalance - convertedToAmount;
             document.getElementById('min_balence').innerText = sum;
+            // if (convertedToNumber === 11) {
+            //     alert('successfull')
+            // }
+
+            // else {
+            //     alert('incorrct your number');
+            // }
+        }
+
+        else {
+            alert('incorrct your pin');
         }
     });
