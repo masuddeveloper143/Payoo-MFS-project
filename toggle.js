bee93270a8318d149d6dffc0b6ac5ea5a54a0869
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-26 12:41:28 +0800

    Update project

---
+++ toggle.js
@@ -0,0 +1,6 @@
+document.getElementById('add-money_box')
+    .addEventListener('click', function () {
+        document.getElementById('first_login_section').style.display = 'none';
+        document.getElementById('cashout').style.display = 'block';
+
+    })
