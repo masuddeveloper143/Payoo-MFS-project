009c87ad5783db97d67eed30bb730422add394b8
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-26 12:10:07 +0800

    Update project

--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 document.getElementById('bangladesh_btn')
     .addEventListener('click', function () {
-        console.log('btn clicked');
+        document.getElementById('bd').style.display = 'block';
+        document.getElementById('usa').style.display = 'none';
 
     });
+
+document.getElementById('usa_btn')
+    .addEventListener('click', function () {
+        document.getElementById('bd').style.display = 'none';
+        document.getElementById('usa').style.display = 'block';
+    })
